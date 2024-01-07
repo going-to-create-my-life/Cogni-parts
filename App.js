51042bf6dd5617de7ea55c9cef20e85defdf534d
going-to-create-my-life <148004949+going-to-create-my-life@users.noreply.github.com>
2024-01-08 02:11:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,133 @@
+// import logo from './logo.svg';
+import './App.css';
+import image from './robo.png';
+
+function App() {
+  return (
+    <>
+    <div className="top">
+      <div id="name">Robotics</div>
+      <div id="overview">Lorem Ipsum Innovwave</div>
+    </div>
+    <div id="main">
+      <img 
+      className="onlyimg" 
+      src={image} 
+      alt=""
+      />
+    <div>
+    <div id="block">
+    <div id="pieces">
+      <div className="piece">
+    <svg width="3.33vw" height="3.33vw" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_528_3521)">
+<path d="M6.36664 0.700439C4.22439 0.700439 2.48157 2.44407 2.48157 4.58706C2.48157 6.64987 6.00656 11.4693 6.15666 11.6734L6.29673 11.8641C6.31311 11.8865 6.3392 11.8996 6.36664 11.8996C6.39451 11.8996 6.42043 11.8865 6.43698 11.8641L6.57697 11.6734C6.72715 11.4693 10.2521 6.64987 10.2521 4.58706C10.2521 2.44407 8.50895 0.700439 6.36664 0.700439ZM6.36664 3.19488C7.13422 3.19488 7.75838 3.81927 7.75838 4.58706C7.75838 5.35445 7.13419 5.97924 6.36664 5.97924C5.59952 5.97924 4.9749 5.35445 4.9749 4.58706C4.9749 3.81927 5.59949 3.19488 6.36664 3.19488Z" fill="#FF5FA2"/>
+</g>
+<defs>
+<clipPath id="clip0_528_3521">
+<rect width="11.1992" height="11.1992" fill="white" transform="translate(0.800049 0.700439)"/>
+</clipPath>
+</defs>
+    </svg>
+    <div className="text_top">IIT Roorkee</div>
+    </div>
+    <div className="piece">
+    <svg width="3.33vw" height="3.33vw" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_528_3527)">
+<path d="M4.99309 4.97632H4.05981V5.90959H4.99309V4.97632Z" fill="#FF5FA2"/>
+<path d="M10.5916 0.774975H10.1249C10.1249 0.517394 9.91634 0.30835 9.65828 0.30835C9.40024 0.30835 9.19165 0.517394 9.19165 0.774975H3.59204C3.59204 0.517394 3.38346 0.30835 3.12541 0.30835C2.86735 0.30835 2.65877 0.517394 2.65877 0.774975H2.19214C1.42033 0.774975 0.792236 1.40307 0.792236 2.17487V2.6415H11.9915V2.17487C11.9915 1.40307 11.3634 0.774975 10.5916 0.774975Z" fill="#FF5FA2"/>
+<path d="M4.99285 8.24194H4.05957V9.17522H4.99285V8.24194Z" fill="#FF5FA2"/>
+<path d="M8.72551 4.97632H7.79224V5.90959H8.72551V4.97632Z" fill="#FF5FA2"/>
+<path d="M0.792236 10.1068C0.792236 10.8786 1.42033 11.5067 2.19214 11.5067H8.11373C7.35824 11.0046 6.85847 10.147 6.85847 9.17357C6.85847 8.20016 7.35824 7.3425 8.11373 6.8404H7.32512C7.06708 6.8404 6.85849 6.63135 6.85849 6.37377V4.50725C6.85849 4.24967 7.06708 4.04062 7.32512 4.04062H9.19165C9.44969 4.04062 9.65828 4.24967 9.65828 4.50725V6.37377C10.6317 6.37377 11.4893 6.87354 11.9915 7.62902V3.57397H0.792236V10.1068ZM3.12541 4.50725C3.12541 4.24967 3.334 4.04062 3.59204 4.04062H5.45857C5.71661 4.04062 5.9252 4.24967 5.9252 4.50725V6.37377C5.9252 6.63135 5.71661 6.8404 5.45857 6.8404H3.59204C3.334 6.8404 3.12541 6.63135 3.12541 6.37377V4.50725ZM3.12541 7.77367C3.12541 7.51609 3.334 7.30705 3.59204 7.30705H5.45857C5.71661 7.30705 5.9252 7.51609 5.9252 7.77367V9.6402C5.9252 9.89778 5.71661 10.1068 5.45857 10.1068H3.59204C3.334 10.1068 3.12541 9.89778 3.12541 9.6402V7.77367Z" fill="#FF5FA2"/>
+<path d="M9.65877 7.30835C8.62938 7.30835 7.79224 8.14549 7.79224 9.17487C7.79224 10.2043 8.62938 11.0414 9.65877 11.0414C10.6882 11.0414 11.5253 10.2043 11.5253 9.17487C11.5253 8.14549 10.6882 7.30835 9.65877 7.30835ZM10.1254 9.64152H9.65879C9.40075 9.64152 9.19216 9.43248 9.19216 9.1749V8.24162C9.19216 7.98404 9.40075 7.775 9.65879 7.775C9.91683 7.775 10.1254 7.98404 10.1254 8.24162V8.70825C10.3835 8.70825 10.592 8.91729 10.592 9.17487C10.592 9.43246 10.3835 9.64152 10.1254 9.64152Z" fill="#FF5FA2"/>
+</g>
+<defs>
+<clipPath id="clip0_528_3527">
+<rect width="11.1992" height="11.1992" fill="white" transform="translate(0.800049 0.300537)"/>
+</clipPath>
+</defs>
+</svg> 
+    <div className="text_top">5-18 January</div>
+    </div>
+    <div className="piece">
+    <svg width="3.33vw" height="3.33vw" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M9.84069 0.699951C9.33216 0.699951 8.71061 1.26499 8.71061 1.83003H4.19029C4.19029 1.26499 3.56875 0.699951 3.06021 0.699951H0.800049V5.7853C0.800049 6.35034 1.36509 6.91538 1.93013 6.91538H3.17322C3.39923 8.04546 4.13379 9.00603 5.88541 9.17554V10.3508C4.19029 10.6107 4.19029 12.0007 4.19029 12.0007H8.71061C8.71061 12.0007 8.71061 10.6107 7.01549 10.3508V9.17554C8.76712 9.00603 9.50167 8.04546 9.72768 6.91538H10.9708C11.5358 6.91538 12.1009 6.35034 12.1009 5.7853V0.699951H9.84069ZM3.06021 5.7853H1.93013V1.83003H3.06021V5.7853ZM10.9708 5.7853H9.84069V1.83003H10.9708V5.7853Z" fill="#FF5FA2"/>
+</svg> 
+    <div className="text_top">Prizes Worth: 5 Lakh+</div>
+    </div>
+    </div>
+    </div>
+    <div id="buttons">
+    <button className="problem">Problem Statement</button>
+    <button className="register">Register</button>
+    </div>
+    </div>
+    </div>
+    <div id="comp">
+      <div id="desc">
+        <div className="heading">
+          Description
+        </div>
+        <div id="desctext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. <br/><br/></div>
+      </div>
+      <div id="stages">
+      <div className="heading">
+      STAGES AND TIMELINES
+        </div>
+        <div class="rounds">
+        <span id="round1">Round 1</span>
+        <span id="line"></span>
+        </div>
+        <div className="round_content">
+          <div className="con">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. 
+        </div>
+    <div className="sum">
+    <div className="info">
+    <div className="tab" style={{width: '25.278vw', height: '6.94vw'}}>
+      <span className="upper">Starts at:<br/></span>
+      <span className="lower">5 Jan, 8 AM IST</span>
+      </div>
+    
+    <div className="tab" style={{width: '33.89vw'}}><span className="upper">Submission Deadline:<br/></span>
+    <span className="lower">12 Jan, 6 PM IST</span>
+    </div>
+    </div>
+    <div className="rbuttons">
+      <button className="problem" >Guidelines</button>
+      <button className="register" >Submission</button>
+    </div>
+    </div>
+        </div>
+        <div style={{marginTop: '4vh'}} class="rounds">
+        <span id="round1">Round 2</span>
+        <span id="line"></span>
+        </div>
+        <div className="round_content">
+          <div className="con">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. 
+        </div>
+    <div className="sum">
+    <div className="info">
+    <div className="tab" style={{width: '25.278vw', height: '6.94vw'}}>
+      <span className="upper">Starts at:<br/></span>
+      <span className="lower">5 Jan, 8 AM IST</span>
+      </div>
+    
+    <div className="tab" style={{width: '33.89vw'}}><span className="upper">Submission Deadline:<br/></span>
+    <span className="lower">12 Jan, 6 PM IST</span>
+    </div>
+    </div>
+    <div className="rbuttons">
+      <button className="problem" >Guidelines</button>
+      <button className="register" >Submission</button>
+    </div>
+    </div>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
+
+
